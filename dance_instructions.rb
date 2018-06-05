dd67d0996b0c90c006c3f318b137eb949b18fca1
persia sarram <kittycuppers01@gmail.com>
2018-06-05 17:29:52 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,12 @@
 
 # Code your methods below
+def feet 
+  puts "start in a worrior position"
+  puts "pick up your left foot"
+  puts "pickup your right foot"
+  puts "put down your left foot then pick it back up again"
+  puts "then pick up your right foot then put it down"
+end 
+def arms 
+  puts "start with both hand to your side"
+  puts "then "
